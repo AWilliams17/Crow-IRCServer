4e20a1ea7f907a6ecd04597b47591440e20451c2
Austin Williams <awilliams17411@gmail.com>
2018-05-12 06:38:44 -0500

    https://www.youtube.com/watch?v=rcjpags7JT8

--- server_modules/room.py
+++ server_modules/room.py
@@ -1,17 +1,3 @@
+
 class Room:
-
-    def __init__(self, name):
-        self.name = name
-        self.users = []
-
-    def add_user(self, user):
-        if user in self.users:
-            print("User already in room {}".format(self.name))
-        else:
-            self.users = self.users + [user]
-
-    def del_user(self, user):
-        if user in self.users:
-            self.users.remove(user)
-        else:
-            print("User not in room {}".format(self.name))
+    pass
--- server_modules/server.py
+++ server_modules/server.py
@@ -3,10 +3,4 @@ from server_modules.irc_protocol import IRCProtocol
 
 
 class ChatServer(Factory):
-
-    def __init__(self):
-        self.users = {}  # maps user names to Chat instances
-        self.rooms = {}  # maps rooms to Chat instances
-
-    def buildProtocol(self, address):
-        return IRCProtocol(self.users, self.rooms)
+    pass
