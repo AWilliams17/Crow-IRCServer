5ca279e88e05a97c78457659246778c2b6ac9e11
Austin Williams <awilliams17411@gmail.com>
2018-05-13 13:16:44 -0500

    major refactoring underway

--- server_modules/irc_protocol.py
+++ server_modules/irc_protocol.py
@@ -1,4 +1,4 @@
-from twisted.words.protocols.irc import IRC, protocol, IRCBadMessage
+from twisted.words.protocols.irc import IRC, protocol
 from server_modules.irc_channel import IRCChannel
 
 
@@ -21,28 +21,35 @@ class IRCProtocol(IRC):
 
     def irc_JOIN(self, prefix, params):
         channel = params[0]
-        user = "{}!{}@{}".format(
-            self.users[self.username][2],
-            self.users[self.username][3],
-            self.users[self.username][4]
-        )
-        if channel not in self.channels:
+
+        """
+        if channel not in self.channels:  # The channel doesn't exist - create it.
             self.channels[channel] = IRCChannel(channel)
 
-        self.topic(self.username, self.channels[channel].channel_name, topic="Test")
-        self.join(user, channel)
+        self.topic(self.username, self.channels[channel].channel_name, topic="Test")  # ToDo: Topics
+
+        self.join(self.users[self.username][5], self.channels[channel].channel_name)
+
         self.channels[channel].users.append(self.users[self.username])
         self.channels[channel].usernames.append(self.users[self.username][1])
         self.channels[channel].nicknames.append(self.users[self.username][2])
 
-        self.names(self.username, self.channels[channel].channel_name, self.channels[channel].nicknames)
+        self.names(self.nickname, self.channels[channel].channel_name, self.channels[channel].nicknames)
+        """
 
     def irc_NICK(self, prefix, params):
         self.nickname = params[0]
 
     def irc_USER(self, prefix, params):
         self.username = params[0]
-        ident = params[1]
-        hostmask = params[2]
-        self.users[self.username] = [self, self.username, self.nickname, ident, hostmask]
+        host = params[2]
+        realname = params[3]
+        hostmask = "{}!{}@{}".format(  # In the format of nickname!username@host
+            self.nickname,
+            self.username,
+            host
+        )
+
+        # ToDo: This can be a dictionary.
+        self.users[self.username] = [self, self.username, self.nickname, realname, host, hostmask]
 
