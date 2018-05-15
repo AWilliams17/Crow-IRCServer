4a926fa70a87ff1c34a45e3bc8301175491e9383
Austin Williams <awilliams17411@gmail.com>
2018-05-15 15:56:14 -0500

    refactored irc_nick

--- server_modules/irc_protocol.py
+++ server_modules/irc_protocol.py
@@ -74,81 +74,36 @@ class IRCProtocol(IRC):
                 if self.users[self].protocol != destination_user_protocol and destination_nickname == destination:
                     destination_user_protocol.privmsg(sender, destination, message)
 
-    # ToDo: ...Refactor this? Probably a lot of this can be used to irc_user
     def irc_NICK(self, prefix, params):
         attempted_nickname = params[0]
-
-        if len(attempted_nickname) > 35:
-            attempted_nickname = attempted_nickname[:35]
-            self.sendLine("Nickname exceeded max char limit(35). It has been trimmed to: {}".format(attempted_nickname))
-
         if self.users[self].hostmask is None:
             self.users[self].hostmask = attempted_nickname
 
-        current_nicknames = []
+        in_use_nicknames = []
         for i in self.users:
-            current_nicknames.append(self.users[i].nickname)
+            in_use_nicknames.append(self.users[i].nickname)
 
         # The nickname is taken.
-        if attempted_nickname in current_nicknames:
+        if attempted_nickname in in_use_nicknames:
             # The user instance has no nickname. This is the case on initial connection.
             if self.users[self].nickname is None:
                 # They've had 2 attempts at changing it - Generate one for them.
-                if self.users[self].nickattempts >= 2:
-                    self.sendLine("Nickname attempts exceeded(2). A random nickname will be generated for you.")
-                    protocol_instance_string = str(self.users[self].protocol).replace(" ", "")
-                    random_nick = ''.join(sample(protocol_instance_string, len(protocol_instance_string)))
-                    random_nick_s = ''.join([c for c in random_nick[:35] if c not in set(".<>_'`")])
-
-                    def validate_nick(nick, current_nicks):
-                        if nick in current_nicknames:
-
-                            def generate_junk(amount):
-                                return ''.join([
-                                    choice(
-                                        ascii_lowercase +
-                                        ascii_uppercase +
-                                        digits) for i in range(amount)
-                                ])
-
-                            # Re shuffle the string + Add random garbage to it and then re-validate it, keep it under 35
-                            nick = (''.join(sample(nick, len(nick))) + generate_junk(15))[:35]
-                            validate_nick(nick, current_nicks)
-                        return nick
-
-                    random_nick_s = validate_nick(random_nick_s, current_nicknames)
-
-                    self.sendLine(":{} NICK {}".format("{}".format(self.users[self].hostmask), random_nick_s))
-                    self.sendLine("Your nickname has been set to a random string based on your unique ID.")
-                    self.users[self].nickname = random_nick_s
-                    self.users[self].hostmask = random_nick_s
-
-                    self.users[self].nickattempts = 0
-                    # If they haven't tried twice yet, give them a chance to set it.
-                else:
+                if self.users[self].nickattempts != 2:
                     self.sendLine(":{} 433 * {} :Nickname is already in use.".format(
                         self.users[self].host, attempted_nickname)
                     )
                     self.users[self].nickattempts += 1
+                else:
+                    self.sendLine("Nickname attempts exceeded(2). A random nickname will be generated for you.")
+                    self.users[self].rename_to_random_nick(in_use_nicknames)
             else:
                 # The user already has a nick, so just send a line telling them its in use and keep things the same.
                 self.sendLine("The nickname {} is already in use.".format(attempted_nickname))
         else:
-            # The user already has connected, therefore he already has a nickname.
-            if self.users[self].nickname is not None:
-                for channel in self.users[self].channels:
-                    channel.rename_user(self.users[self], attempted_nickname)
-                self.sendLine(":{} NICK {}".format("{}".format(self.users[self].hostmask), attempted_nickname))
+            try:
                 self.users[self].nickname = attempted_nickname
-                self.users[self].hostmask = self.users[self].nickname
-
-            if self.users[self].nickattempts != 0:
-                # This is their first connection: they recently tried an invalid nick, so tell them this one is accepted
-                self.sendLine(":{} NICK {}".format("{}".format(self.users[self].hostmask), attempted_nickname))
-                self.users[self].nickattempts = 0
-            # ToDo: Fix this
-            self.users[self].nickname = attempted_nickname
-            self.users[self].hostmask = self.users[self].nickname
+            except ValueError as err:
+                self.sendLine(str(err))
 
     def irc_USER(self, prefix, params):
         self.users[self].username = params[0]
--- server_modules/irc_user.py
+++ server_modules/irc_user.py
@@ -1,8 +1,12 @@
+from random import sample, choice
+from string import ascii_lowercase, ascii_uppercase, digits
+
+
 class IRCUser:
     def __init__(self, protocol, username, nickname, realname, host, hostmask, channels, nickattempts):
         self.protocol = protocol
         self.username = username
-        self._nickname = nickname
+        self.__nickname = nickname
         self.realname = realname
         self.host = host
         self.__hostmask = hostmask
@@ -26,12 +30,51 @@ class IRCUser:
 
     @property
     def nickname(self):
-        return self._nickname
+        return self.__nickname
 
     @nickname.setter
     def nickname(self, desired_nickname):
         if len(desired_nickname) > 35:
-            raise ValueError("Error: Nickname exceeded max char limit (35).")
+            error = "Error: Nickname exceeded max char limit (35)."
+            if self.__nickname is None:
+                error = error + " Use /nick to set a new nick."
+            raise ValueError(error)
 
+        if self.__nickname is not None:
+            if self.channels is not None:
+                for connected_channel in self.channels:
+                    connected_channel.rename_user(self, desired_nickname)
+            self.protocol.sendLine(":{} NICK {}".format(self.hostmask, desired_nickname))
 
+        if self.nickattempts != 0:
+            if self.__nickname is not None:
+                self.nickattempts = 0
+            self.protocol.sendLine(":{} NICK {}".format(self.hostmask, desired_nickname))
+
+        self.__nickname = desired_nickname
+        self.hostmask = desired_nickname
+
+    def rename_to_random_nick(self, current_nicknames):
+        protocol_instance_string = str(self.protocol).replace(" ", "")
+        random_nick = ''.join(sample(protocol_instance_string, len(protocol_instance_string)))
+        random_nick_s = ''.join([c for c in random_nick[:35] if c not in set(".<>_'`")])
+
+        def validate_nick(nick, current_nicks):
+            if nick in current_nicknames:
+                def generate_junk(amount):
+                    return ''.join([
+                        choice(
+                            ascii_lowercase +
+                            ascii_uppercase +
+                            digits) for i in range(amount)
+                    ])
+
+                # Re shuffle the string + Add random garbage to it and then re-validate it, keep it under 35
+                nick = (''.join(sample(nick, len(nick))) + generate_junk(15))[:35]
+                validate_nick(nick, current_nicks)
+            return nick
+
+        random_nick_s = validate_nick(random_nick_s, current_nicknames)
+        self.nickname = random_nick_s
+        self.protocol.sendLine("Your nickname has been set to a random string based on your unique ID.")
 
