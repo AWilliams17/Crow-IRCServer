d0e406072aa48133dd78d812e6df088a3c3b5f13
Austin Williams <awilliams17411@gmail.com>
2018-05-25 14:10:59 -0500

    added flush_server stub, added delete_old_channels stub in irc_server

--- server_modules/irc_server.py
+++ server_modules/irc_server.py
@@ -14,9 +14,17 @@ class ChatServer(Factory):
         self.config = config
 
     def do_maintenance(self):
-        """ This method gets called every 5 minutes as defined in main.py, so any maintenance methods called here will
+        """ This method gets called every x minutes as defined in crow.ini, so any maintenance methods called here will
          be called. """
         self.ratelimiter.maintenance()
+        # self.delete_old_channels() - ToDo: if specified in config, delete old channels.
+        # (channels which have not had anyone on them, including owner in atleast a week)
+
+    def flush_server(self):
+        """ This method gets called every 30 minutes (hard coded). The purpose of it is to save current channels and
+        their modes, owner details, oper details, and banlists to the server which will be reloaded on
+        server restart."""
+        pass
 
     def buildProtocol(self, addr):
         return IRCProtocol(self.users, self.channels, self.config, self.ratelimiter, self.clientlimiter)
