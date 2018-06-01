d81db31ac44441d55ebfb18bcd392fe0662cd769
Austin Williams <awilliams17411@gmail.com>
2018-06-01 05:31:26 -0500

    why

--- server/irc_config/config.py
+++ server/irc_config/config.py
@@ -1,26 +1,28 @@
 from utils.sentry.sentry_config import *
 from utils.sentry.sentry_type_converters import *
 from server.irc_config.validators import *
+from server.irc_config.option_descriptions import *
 
 
 class IRCConfig(SentryConfig):
     """ Represents the server configuration file. """
     class ServerSettings(SentrySection):
-        Port = SentryOption(6667, IntConverter)
-        Interface = SentryOption("127.0.0.1")
-        PingInterval = SentryOption(3, IntConverter)
-        ServerName = SentryOption("Crow IRC")
-        ServerDescription = SentryOption("WIP IRC Server implementation w/ Twisted.")
-        ServerWelcome = SentryOption("Welcome to Crow IRC")
+        Port = SentryOption(6667, IntConverter, PortDescription)
+        Interface = SentryOption("127.0.0.1", description=InterfaceDescription)
+        PingInterval = SentryOption(3, IntConverter, PingIntervalDescription)
+        ServerName = SentryOption("Crow IRC", description=ServerNameDescription)
+        ServerDescription = SentryOption("WIP IRC Server implementation w/ Twisted.", description=ServerNameDescription)
+        ServerWelcome = SentryOption("Welcome to Crow IRC", description=ServerWelcomeDescription)
 
     class MaintenanceSettings(SentrySection):
-        RateLimitClearInterval = SentryOption(5, IntConverter)
-        FlushInterval = SentryOption(1, IntConverter)
-        ChannelScanInterval = SentryOption(1, IntConverter)
-        ChannelUltimatum = SentryOption(7, IntConverter)
+        RateLimitClearInterval = SentryOption(5, IntConverter, RateLimitClearIntervalDescription)
+        FlushInterval = SentryOption(1, IntConverter, FlushIntervalDescription)
+        ChannelScanInterval = SentryOption(1, IntConverter, ChannelScanIntervalDescription)
+        ChannelUltimatum = SentryOption(7, IntConverter, ChannelUltimatumDescription)
 
     class UserSettings(SentrySection):
-        MaxUsernameLength = SentryOption(35, [IntConverter, MaxUsernameLengthCriteria])
-        MaxNicknameLength = SentryOption(35, [IntConverter, MaxNicknameLengthCriteria])
-        MaxClients = SentryOption(5, [IntConverter, MaxClientsCriteria])
-        Operators = SentryOption({"Admin": "Password", "Admin2": "Password2"}, DictConverter)  # lol
+        MaxUsernameLength = SentryOption(35, [IntConverter, MaxUsernameLengthCriteria], MaxUsernameLengthDescription)
+        MaxNicknameLength = SentryOption(35, [IntConverter, MaxNicknameLengthCriteria], MaxNicknameLengthDescription)
+        MaxClients = SentryOption(5, [IntConverter, MaxClientsCriteria], MaxClientsDescription)
+        Operators = SentryOption({"Admin": "Password", "Admin2": "Password2"}, DictConverter, OperatorsDescription)
+        # lol ^
---
+++ server/irc_config/option_descriptions.py
@@ -0,0 +1,60 @@
+#  yep I am doing this I guess.
+#  why am I doing this? because I don't want it all in config.py :#
+
+
+# ServerSettings option descriptions
+PortDescription = "This is the port the server will listen on. Common IRC ports are 6667,6668,6669, and 7000," \
+                  " with 6697 being for SSL (not implemented). The default port of 6667 should suffice."
+
+InterfaceDescription = "What network interface to bind to. 127.0.0.1 is localhost. " \
+                       "To listen on all interfaces, use 0.0.0."
+
+PingIntervalDescription = "How often clients connected to the server should be pinged, in minutes. " \
+                          "Default value is 3. If set to 0, clients will not be pinged. (they won't be timed out.)"
+
+ServerNameDescription = "The name communicated to the client on initial connection."
+
+ServerDescription = "Describes the server. At the moment it is only used in WHOIS replies."
+
+ServerWelcomeDescription = "The welcome message echoed to the client on initial connection. " \
+                "The client's nickname is appended to the end of it."
+
+
+# MaintenanceSettings option descriptions
+RateLimitClearIntervalDescription = "How much time in minutes to wait before clearing old entries in the " \
+                                    "rate limiter dictionary. Default is every 5 minutes. If set to 0, then " \
+                                    "ratelimiter entries will not be removed. (not recommended)"
+
+FlushIntervalDescription = "(Not Implemented)FlushInterval: How much time in hours to wait before flushing " \
+                           "current server details (current channels + their opers, owner, modes, and banlists) + " \
+                           "the server's own banlist. The default value is 1 hour. " \
+                           "If set to 0, these will not be saved."
+
+ChannelScanIntervalDescription = "How many days to wait before running the old channel scan. " \
+                                 "Default value is 1. If the interval is 0, then old channels will not be removed."
+
+# christ almighty
+ChannelUltimatumDescription = "(If ChannelScanInterval is 0, this value will not be used) This controls how long a " \
+                              "channel must have no owner logged in before being considered for deletion. " \
+                              "For example, if set to 7 days, then when the owner logs out and the channel scan " \
+                              "is initiated, then the last owner login date is compared against this value. " \
+                              "If there are less than 3 days left before the channel is slated for deletion, " \
+                              "then a notice will be sent to the channel every time the channel scan is initiated " \
+                              "until there are no days remaining, where it is then slated for " \
+                              "deletion on the next scan. If an owner logs in, then this all of this is reset. " \
+                              "Also, once a channel is slated for deletion on the next scan, if an owner logs in " \
+                              "before the scan occurs, then the channel will be saved."
+
+
+# UserSettings option descriptions
+MaxUsernameLengthDescription = "The maximum amount of characters a username can be."
+
+MaxNicknameLengthDescription = "The maximum amount of characters a nickname can be."
+
+MaxClientsDescription = "How many clients a host can have connected at the same time."
+
+OperatorsDescription = "A list of operator usernames and their associated passwords. " \
+                       "Separate each username:password pair with a comma. " \
+                       "Note, if a username is the same as a previous username, " \
+                       "that previous username will have it's password value replaced " \
+                       "by the subsequent username's associated password."
