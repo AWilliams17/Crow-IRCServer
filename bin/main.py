c9d0b6bb3c72ec0c4c71d89c41fa8871491e0e48
Austin Williams <awilliams17411@gmail.com>
2018-06-02 00:08:35 -0500

    now using serverFromString for endpoint

--- bin/main.py
+++ bin/main.py
@@ -1,14 +1,14 @@
 from server.irc_config.config import IRCConfig
 from server.irc_server import ChatServer
 from twisted.internet import reactor, task
-from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.endpoints import serverFromString
 from os import getcwd, path
 
 import twisted.internet.defer
 twisted.internet.defer.setDebugging(True)
 
 
-def setup_loopingcalls(server, maintenance_settings, server_settings):
+def setup_loopingcalls(server, maintenance_settings):
 	ratelimitclearinterval = maintenance_settings.RateLimitClearInterval * 60
 	flushinterval = maintenance_settings.FlushInterval * 3600
 	channelscaninterval = maintenance_settings.ChannelScanInterval * 86400
@@ -33,16 +33,15 @@ if __name__ == '__main__':
 	if not path.exists(ini_path):
 		server_config.flush_config()
 	config_output = server_config.read_config()
+	server_settings = server_config.ServerSettings  # just for convenience
 
-	if config_output is not None:
+	if config_output is not None:  # config output is not yet implemented so this does nothing
 		for output in config_output:
 			print(output)
 
 	server_instance = ChatServer(server_config)
-	setup_loopingcalls(server_instance, server_config.MaintenanceSettings, server_config.ServerSettings)
+	setup_loopingcalls(server_instance, server_config.MaintenanceSettings)
 
-	endpoint = TCP4ServerEndpoint(
-		reactor, port=server_config.ServerSettings.Port, interface=server_config.ServerSettings.Interface
-	)
+	endpoint = serverFromString(reactor, "tcp:{}:interface={}".format(server_settings.Port, server_settings.Interface))
 	endpoint.listen(server_instance)
 	reactor.run()
