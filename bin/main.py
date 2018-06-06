29ad8893c875a25b2a0d3056af221f9917291a06
Austin Williams <awilliams17411@gmail.com>
2018-06-06 01:21:07 -0500

    SSLPort done

--- bin/main.py
+++ bin/main.py
@@ -31,18 +31,19 @@ def setup_loopingcalls(server, maintenance_settings):
 def create_ssl_endpoint(ssl_settings):
 	if server_config.SSLSettings.SSLEnabled:
 		print("SSLEnabled is True - Attempting to construct an SSL Endpoint...")
+		ssl_port = ssl_settings.SSLPort
 		ssl_key = ssl_settings.SSLKeyPath
 		ssl_cert = ssl_settings.SSLCertPath
 		if ssl_key is None or ssl_cert is None:
 			print("Error constructing SSL Endpoint: Missing key/cert file.")
 		else:
-			ssl_endpoint = serverFromString(reactor, "ssl:6697:privateKey={}:certKey={}".format(ssl_key, ssl_cert))
+			ssl_endpoint = serverFromString(reactor, "ssl:{}:privateKey={}:certKey={}".format(ssl_port, ssl_key, ssl_cert))
 			ssl_endpoint.listen(server_instance)
 			print("SSL Endpoint is now listening on port 6697.")
 
 
 if __name__ == '__main__':
-	if getuid() == 0:
+	if getuid() == 0:  # Prevent from running as root
 		print("Error: You can not run this application as root.")
 
 	ini_path = getcwd().strip("bin") + "/crow.ini"
