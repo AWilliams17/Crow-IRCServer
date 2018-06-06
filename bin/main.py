0fdfb29d74a3579fbec07d22dcd7bc622e7fc64e
Austin Williams <awilliams17411@gmail.com>
2018-06-06 02:45:34 -0500

    now taking into account ssl_only

--- bin/main.py
+++ bin/main.py
@@ -9,7 +9,7 @@ import twisted.internet.defer
 twisted.internet.defer.setDebugging(True)
 
 
-def setup_loopingcalls(server, maintenance_settings):
+def setup_loopingcalls(server, server_settings, maintenance_settings):
 	ratelimitclearinterval = maintenance_settings.RateLimitClearInterval * 60
 	flushinterval = maintenance_settings.FlushInterval * 3600
 	channelscaninterval = maintenance_settings.ChannelScanInterval * 86400
@@ -28,18 +28,29 @@ def setup_loopingcalls(server, maintenance_settings):
 		task.LoopingCall(server.do_pings).start(pinginterval)
 
 
-def create_ssl_endpoint(ssl_settings):
+def create_endpoints(server, server_settings, ssl_settings):
+	port = server_settings.Port
+	interface = server_settings.Interface
+	ssl_port = ssl_settings.SSLPort
+	ssl_key = ssl_settings.SSLKeyPath
+	ssl_cert = ssl_settings.SSLCertPath
+	ssl_only = ssl_settings.SSLOnly
 	if server_config.SSLSettings.SSLEnabled:
 		print("SSLEnabled is True - Attempting to construct an SSL Endpoint...")
-		ssl_port = ssl_settings.SSLPort
-		ssl_key = ssl_settings.SSLKeyPath
-		ssl_cert = ssl_settings.SSLCertPath
 		if ssl_key is None or ssl_cert is None:
 			print("Error constructing SSL Endpoint: Missing key/cert file.")
+			if ssl_only:
+				print("Error: SSLOnly is enabled and no SSL endpoint was created, so there is no endpoint. Terminating.")
+				exit()
 		else:
 			ssl_endpoint = serverFromString(reactor, "ssl:{}:privateKey={}:certKey={}".format(ssl_port, ssl_key, ssl_cert))
-			ssl_endpoint.listen(server_instance)
+			ssl_endpoint.listen(server)
 			print("SSL Endpoint is now listening on port 6697.")
+	if not ssl_only:
+		print("Creating endpoint for port '{}', interface '{}'".format(port, interface))
+		endpoint = serverFromString(reactor, "tcp:{}:interface={}".format(port, interface))
+		endpoint.listen(server)
+		print("Endpoint is now listening on port '{}'".format(port))
 
 
 if __name__ == '__main__':
@@ -51,16 +62,14 @@ if __name__ == '__main__':
 	if not path.exists(ini_path):
 		server_config.flush_config()
 	config_output = server_config.read_config()
-	server_settings = server_config.ServerSettings  # just for convenience
 
 	if config_output is not None:  # config output is not yet implemented so this does nothing
 		for output in config_output:
 			print(output)
 
 	server_instance = ChatServer(server_config)
-	setup_loopingcalls(server_instance, server_config.MaintenanceSettings)
 
-	create_ssl_endpoint(server_config.SSLSettings)
-	endpoint = serverFromString(reactor, "tcp:{}:interface={}".format(server_settings.Port, server_settings.Interface))
-	endpoint.listen(server_instance)
+	setup_loopingcalls(server_instance, server_config.ServerSettings, server_config.MaintenanceSettings)
+	create_endpoints(server_instance, server_config.ServerSettings, server_config.SSLSettings)
+
 	reactor.run()
