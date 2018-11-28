61fb6f28e36e28af447c670b6a648d68f69fed28
AWilliams17 <awilliams17411@gmail.com>
2018-11-28 15:01:00 -0600

    Setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Crow-IRCServer',
+    version='1.0.0',
+    description='IRC Server implemented in Python via the Twisted framework.',
+    url='https://github.com/AWilliams17/Crow-IRCServer/',
+    author='Austin Williams',
+    author_email='awilliams17412@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Framework :: Twisted'
+        'License :: Freeware'
+        'Topic :: Communications :: Chat :: Internet Relay Chat'
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='flask pastebin flask-vue flask-pastebin',
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=['twisted'],
+
+    entry_points={
+        'console_scripts': [
+            'server=bin.main:main',
+        ],
+    },
+
+    project_urls={
+        'Bug Reports': 'https://github.com/AWilliams17/Crow-IRCServer/issues',
+        'Source': 'https://github.com/AWilliams17/Crow-IRCServer/',
+    },
+)
